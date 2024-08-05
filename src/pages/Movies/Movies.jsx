890a72b95907748f2f2b7e2348cdb428e4a4f630
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-08-05 12:00:58 +0300

    alfa version

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,8 @@
+import { Link, useLocation } from 'react-router-dom';
+import css from './ProductList.module.css';
+
+const Movies = () => {
+  return <div>Movies</div>;
+};
+
+export default Movies;
