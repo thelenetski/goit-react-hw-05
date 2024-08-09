18be78c896bbcb4dcc450823a70497e4a1a7bec4
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-08-09 21:15:26 +0300

    change some css

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -35,62 +35,64 @@ const MovieDetailsPage = () => {
     <main className={css.mainMovie}>
       <BackLink to={backLinkHref.current}>Back</BackLink>
       {loading && <Loader />}
-      {!loading && (
-        <>
-          {data.poster_path ? (
-            <img
-              src={IMG_LINK + data.poster_path}
-              alt={data.original_title}
-              className={css.moviePoster}
-            />
-          ) : (
-            <FaRegFileImage className={css.posterSVG} />
-          )}
-
-          <div className={css.movieDescription}>
-            <h2>{data.original_title}</h2>
-            <span>User score: {Math.round(data.vote_average * 10)}%</span>
-            <span>Release: {data.release_date}</span>
-            <div>
-              {data.production_countries.length > 0 && (
-                <div>
-                  <span>Country: </span>
-                  {data.production_countries.map((item, index) => {
-                    return <span key={index}>{item.name}</span>;
-                  })}
-                </div>
-              )}
-            </div>
-            {data.overview && (
-              <>
-                <h4>Overview</h4>
-                <p>{data.overview}</p>
-              </>
+      <div className={css.detailsWrap}>
+        {!loading && (
+          <>
+            {data.poster_path ? (
+              <img
+                src={IMG_LINK + data.poster_path}
+                alt={data.original_title}
+                className={css.moviePoster}
+              />
+            ) : (
+              <FaRegFileImage className={css.posterSVG} />
             )}
-            <h4>Genres</h4>
-            <div className={css.genresBox}>
-              {data.genres.map(item => {
-                return <p key={item.id}>{item.name}</p>;
-              })}
-            </div>
-          </div>
-          <div className={css.addInfo}>
-            <h4>Additional information</h4>
-            <ul>
-              <li>
-                <Link to="cast">Cast</Link>
-              </li>
-              <li>
-                <Link to="reviews">Reviews</Link>
-              </li>
-            </ul>
 
-            <Suspense fallback={<div>Loading subpage...</div>}>
-              <Outlet />
-            </Suspense>
-          </div>
-        </>
-      )}
+            <div className={css.movieDescription}>
+              <h2>{data.original_title}</h2>
+              <span>User score: {Math.round(data.vote_average * 10)}%</span>
+              <span>Release: {data.release_date}</span>
+              <div>
+                {data.production_countries.length > 0 && (
+                  <div>
+                    <span>Country: </span>
+                    {data.production_countries.map((item, index) => {
+                      return <span key={index}>{item.name}</span>;
+                    })}
+                  </div>
+                )}
+              </div>
+              {data.overview && (
+                <>
+                  <h4>Overview</h4>
+                  <p>{data.overview}</p>
+                </>
+              )}
+              <h4>Genres</h4>
+              <div className={css.genresBox}>
+                {data.genres.map(item => {
+                  return <p key={item.id}>{item.name}</p>;
+                })}
+              </div>
+            </div>
+            <div className={css.addInfo}>
+              <h4>Additional information</h4>
+              <ul>
+                <li>
+                  <Link to="cast">Cast</Link>
+                </li>
+                <li>
+                  <Link to="reviews">Reviews</Link>
+                </li>
+              </ul>
+
+              <Suspense fallback={<div>Loading subpage...</div>}>
+                <Outlet />
+              </Suspense>
+            </div>
+          </>
+        )}
+      </div>
     </main>
   );
 };
