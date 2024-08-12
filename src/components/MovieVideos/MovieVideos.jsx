952f87f5981bcf120c2ea881df90f51b9f98ad46
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-08-12 15:14:24 +0300

    fix height videos

--- src/components/MovieVideos/MovieVideos.jsx
+++ src/components/MovieVideos/MovieVideos.jsx
@@ -40,7 +40,7 @@ const MovieVideos = () => {
                     {
                       <iframe
                         width="560"
-                        height="315"
+                        // height="315"
                         src={`https://www.youtube.com/embed/${item.key}`}
                         title="YouTube video player"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
