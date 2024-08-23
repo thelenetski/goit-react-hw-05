46f76e0120644fc1ba4b8dd5eb5f34cd3499c267
thelenetski <49438367+thelenetski@users.noreply.github.com>
2024-08-23 14:46:54 +0300

    fix rate

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -30,7 +30,7 @@ const buildLinkClass = ({ isActive }) => {
 const buildRateClass = rate => {
   return clsx(
     rate < 59 && css.rateBad,
-    (rate < 68 && css.rateNorm) || (rate > 69 && css.rateNice)
+    (rate < 69 && css.rateNorm) || (rate > 69 && css.rateNice)
   );
 };
 
