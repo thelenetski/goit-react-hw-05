32c00f4108b8ea16a295f83d0695837d90b9dd97
lashmaker <thelenetski@gmail.com>
2026-03-23 15:49:14 +0200

    fix review sort

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -22,8 +22,9 @@ const MovieReviews = () => {
   const error = useSelector(selectError);
   const sortedReviews =
     data.results &&
-    [...data.results].sort(
-      (a, b) => +new Date(b.updated_at) - +new Date(a.updated_at)
+    [...data.results]?.sort(
+      (a, b) =>
+        new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
     );
 
   useEffect(() => {
